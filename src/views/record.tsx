12564ee4c048b7840ee119423a5ea710f738d8a9
Juliet <m@juli.ee>
2025-03-02 13:53:46 -0800

    wip

--- src/views/record.tsx
+++ src/views/record.tsx
@@ -17,22 +17,7 @@ import {
 } from "../utils/api.js";
 import { theme } from "../components/settings.jsx";
 import { AtUri, uriTemplates } from "../utils/templates.js";
-
-const wasmSupported = (() => {
-  try {
-    if (
-      typeof WebAssembly === "object" &&
-      typeof WebAssembly.instantiate === "function"
-    ) {
-      const module = new WebAssembly.Module(
-        Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00),
-      );
-      if (module instanceof WebAssembly.Module)
-        return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
-    }
-  } catch (e) {}
-  return false;
-})();
+import { wasmSupported } from "../utils/wasm.js";
 
 export default () => {
   const params = useParams();
