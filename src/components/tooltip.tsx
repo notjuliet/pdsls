f65d9027bc3606c4f9c001309a25233aca4fa3e6
Juliet <m@juli.ee>
2025-07-26 04:28:08 +0200

    adjust tooltip position

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -9,7 +9,7 @@ const Tooltip = (props: { text: string; children: JSX.Element }) => {
       {props.children}
       <Show when={!isTouchDevice}>
         <span
-          style={`transform: translate(-50%, 2rem); min-width: ${width}ch`}
+          style={`transform: translate(-50%, 28px); min-width: ${width}ch`}
           class={`left-50% border-0.5 pointer-events-none absolute z-10 hidden select-none whitespace-nowrap rounded border-neutral-300 bg-zinc-100 p-1 text-center font-sans text-xs text-slate-900 shadow-md group-hover/tooltip:inline dark:border-neutral-600 dark:bg-neutral-800 dark:text-slate-100`}
         >
           {props.text}
