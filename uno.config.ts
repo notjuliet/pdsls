28b6e2cd21432a3069a405edf4fce7fde3ba7066
Juliet <m@juli.ee>
2025-07-29 20:05:25 +0200

    switch to inter & jetbrains mono

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,15 @@
-import { defineConfig, presetIcons, presetWind4 } from "unocss";
+import { defineConfig, presetIcons, presetWebFonts, presetWind4 } from "unocss";
 
 export default defineConfig({
-  presets: [presetIcons(), presetWind4()],
+  presets: [
+    presetIcons(),
+    presetWind4(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {
+        sans: "Inter:400,600,700",
+        mono: "JetBrains Mono:400,700",
+      },
+    }),
+  ],
 });
