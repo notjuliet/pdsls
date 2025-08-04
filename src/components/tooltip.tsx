bf6c82d5740760784420b915103eed253c9c8b97
Juliet <m@juli.ee>
2025-08-04 18:27:53 +0200

    plc logs filters

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -2,21 +2,18 @@ import { JSX, Show } from "solid-js";
 
 const isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 1;
 
-const Tooltip = (props: { text: string; children: JSX.Element }) => {
-  const width = (props.text.length - 1).toString();
-  return (
-    <div class="group/tooltip relative flex items-center">
-      {props.children}
-      <Show when={!isTouchDevice}>
-        <span
-          style={`transform: translate(-50%, 28px); min-width: ${width}ch`}
-          class={`left-50% border-0.5 dark:shadow-dark-900 pointer-events-none absolute z-10 hidden select-none whitespace-nowrap rounded border-neutral-300 bg-white p-1 text-center font-sans text-xs text-slate-900 shadow-md group-hover/tooltip:inline dark:border-neutral-600 dark:bg-neutral-800 dark:text-slate-100`}
-        >
-          {props.text}
-        </span>
-      </Show>
-    </div>
-  );
-};
+const Tooltip = (props: { text: string; children: JSX.Element }) => (
+  <div class="group/tooltip relative flex items-center">
+    {props.children}
+    <Show when={!isTouchDevice}>
+      <span
+        style={`transform: translate(-50%, 28px)`}
+        class={`left-50% border-0.5 dark:shadow-dark-900 pointer-events-none absolute z-10 hidden min-w-fit select-none whitespace-nowrap rounded border-neutral-300 bg-white p-1 text-center font-sans text-xs text-slate-900 shadow-md group-hover/tooltip:inline dark:border-neutral-600 dark:bg-neutral-800 dark:text-slate-100`}
+      >
+        {props.text}
+      </span>
+    </Show>
+  </div>
+);
 
 export default Tooltip;
--- src/views/repo.tsx
+++ src/views/repo.tsx
@@ -21,6 +21,125 @@ import { createOperationHistory, DiffEntry, groupBy } from "../utils/plc-logs.js
 import { localDateFromTimestamp } from "../utils/date.js";
 
 type Tab = "collections" | "backlinks" | "doc" | "blobs";
+type PlcEvent = "handle" | "rotation_key" | "service" | "verification_method";
+
+const PlcLogView = (props: {
+  plcOps: [IndexedEntry<CompatibleOperationOrTombstone>, DiffEntry[]][];
+}) => {
+  const [activePlcEvent, setActivePlcEvent] = createSignal<PlcEvent | undefined>();
+
+  const FilterButton = (props: { icon: string; event: PlcEvent }) => (
+    <button
+      classList={{
+        "rounded-full p-1.5": true,
+        "bg-neutral-700 dark:bg-neutral-200": activePlcEvent() === props.event,
+      }}
+      onclick={() => setActivePlcEvent(activePlcEvent() === props.event ? undefined : props.event)}
+    >
+      <div
+        class={`${props.icon} text-xl ${activePlcEvent() === props.event ? "text-slate-100 dark:text-slate-900" : ""}`}
+      />
+    </button>
+  );
+
+  const DiffItem = (props: { diff: DiffEntry }) => {
+    const diff = props.diff;
+    let title = "Unknown log entry";
+    let icon = "i-lucide-circle-help";
+    let value = "";
+
+    if (diff.type === "identity_created") {
+      icon = "i-lucide-bell";
+      title = `Identity created`;
+    } else if (diff.type === "identity_tombstoned") {
+      icon = "i-lucide-skull";
+      title = `Identity tombstoned`;
+    } else if (diff.type === "handle_added" || diff.type === "handle_removed") {
+      icon = "i-lucide-at-sign";
+      title = diff.type === "handle_added" ? "Alias added" : "Alias removed";
+      value = diff.handle;
+    } else if (diff.type === "handle_changed") {
+      icon = "i-lucide-at-sign";
+      title = "Alias updated";
+      value = `${diff.prev_handle} → ${diff.next_handle}`;
+    } else if (diff.type === "rotation_key_added" || diff.type === "rotation_key_removed") {
+      icon = "i-lucide-key-round";
+      title = diff.type === "rotation_key_added" ? "Rotation key added" : "Rotation key removed";
+      value = diff.rotation_key;
+    } else if (diff.type === "service_added" || diff.type === "service_removed") {
+      icon = "i-lucide-server";
+      title = `Service ${diff.service_id} ${diff.type === "service_added" ? "added" : "removed"}`;
+      value = `${diff.service_endpoint}`;
+    } else if (diff.type === "service_changed") {
+      icon = "i-lucide-server";
+      title = `Service ${diff.service_id} updated`;
+      value = `${diff.prev_service_endpoint} → ${diff.next_service_endpoint}`;
+    } else if (
+      diff.type === "verification_method_added" ||
+      diff.type === "verification_method_removed"
+    ) {
+      icon = "i-lucide-shield-check";
+      title = `Verification method ${diff.method_id} ${diff.type === "verification_method_added" ? "added" : "removed"}`;
+      value = `${diff.method_key}`;
+    } else if (diff.type === "verification_method_changed") {
+      icon = "i-lucide-shield-check";
+      title = `Verification method ${diff.method_id} updated`;
+      value = `${diff.prev_method_key} → ${diff.next_method_key}`;
+    }
+
+    return (
+      <div class="grid grid-cols-[min-content_1fr] items-center gap-x-1">
+        <div class={icon + ` shrink-0 text-lg`} />
+        <p
+          classList={{
+            "font-semibold": true,
+            "text-gray-500 line-through dark:text-gray-400": diff.orig.nullified,
+          }}
+        >
+          {title}
+        </p>
+        <div></div>
+        {value}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div class="flex items-center gap-1">
+        <Tooltip text="Filter operations">
+          <div class="i-lucide-filter text-xl" />
+        </Tooltip>
+        <div class="dark:shadow-dark-900 flex w-fit items-center rounded-full bg-neutral-200 shadow-md dark:bg-neutral-700">
+          <FilterButton icon="i-lucide-at-sign" event="handle" />
+          <FilterButton icon="i-lucide-key-round" event="rotation_key" />
+          <FilterButton icon="i-lucide-server" event="service" />
+          <FilterButton icon="i-lucide-shield-check" event="verification_method" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-1 text-sm">
+        <For each={props.plcOps}>
+          {([entry, diffs]) => (
+            <Show
+              when={!activePlcEvent() || diffs.find((d) => d.type.startsWith(activePlcEvent()!))}
+            >
+              <div class="flex flex-col">
+                <span class="text-neutral-500 dark:text-neutral-400">
+                  {localDateFromTimestamp(new Date(entry.createdAt).getTime())}
+                </span>
+                {diffs.map((diff) => (
+                  <Show when={!activePlcEvent() || diff.type.startsWith(activePlcEvent()!)}>
+                    <DiffItem diff={diff} />
+                  </Show>
+                ))}
+              </div>
+            </Show>
+          )}
+        </For>
+      </div>
+    </>
+  );
+};
 
 const RepoView = () => {
   const params = useParams();
@@ -51,81 +170,6 @@ const RepoView = () => {
       {props.label}
     </button>
   );
-
-  const DiffItem = (props: { diff: DiffEntry }) => {
-    const diff = props.diff;
-    let title = "Unknown log entry";
-    let icon = "i-lucide-circle-help";
-    let value = "";
-
-    if (diff.type === "identity_created") {
-      icon = "i-lucide-bell";
-      title = `Identity created`;
-    } else if (diff.type === "identity_tombstoned") {
-      icon = "i-lucide-skull";
-      title = `Identity tombstoned`;
-    } else if (diff.type === "handle_added") {
-      icon = "i-lucide-at-sign";
-      title = "Alias added";
-      value = diff.handle;
-    } else if (diff.type === "handle_changed") {
-      icon = "i-lucide-at-sign";
-      title = "Alias updated";
-      value = `${diff.prev_handle} → ${diff.next_handle}`;
-    } else if (diff.type === "handle_removed") {
-      icon = "i-lucide-at-sign";
-      title = `Alias removed`;
-      value = diff.handle;
-    } else if (diff.type === "rotation_key_added") {
-      icon = "i-lucide-key-round";
-      title = `Rotation key added`;
-      value = diff.rotation_key;
-    } else if (diff.type === "rotation_key_removed") {
-      icon = "i-lucide-key-round";
-      title = `Rotation key removed`;
-      value = diff.rotation_key;
-    } else if (diff.type === "service_added") {
-      icon = "i-lucide-server";
-      title = `Service ${diff.service_id} added`;
-      value = `${diff.service_endpoint}`;
-    } else if (diff.type === "service_changed") {
-      icon = "i-lucide-server";
-      title = `Service ${diff.service_id} updated`;
-      value = `${diff.prev_service_endpoint} → ${diff.next_service_endpoint}`;
-    } else if (diff.type === "service_removed") {
-      icon = "i-lucide-server";
-      title = `Service ${diff.service_id} removed`;
-      value = `${diff.service_endpoint}`;
-    } else if (diff.type === "verification_method_added") {
-      icon = "i-lucide-shield-check";
-      title = `Verification method ${diff.method_id} added`;
-      value = `${diff.method_key}`;
-    } else if (diff.type === "verification_method_changed") {
-      icon = "i-lucide-shield-check";
-      title = `Verification method ${diff.method_id} updated`;
-      value = `${diff.prev_method_key} → ${diff.next_method_key}`;
-    } else if (diff.type === "verification_method_removed") {
-      icon = "i-lucide-shield-check";
-      title = `Verification method ${diff.method_id} removed`;
-      value = `${diff.method_key}`;
-    }
-
-    return (
-      <div class="grid grid-cols-[min-content_1fr] items-center gap-x-1">
-        <div class={icon + ` shrink-0 text-lg`} />
-        <p
-          classList={{
-            "font-semibold": true,
-            "text-gray-500 line-through dark:text-gray-400": diff.orig.nullified,
-          }}
-        >
-          {title}
-        </p>
-        <div></div>
-        {value}
-      </div>
-    );
-  };
 
   const fetchRepo = async () => {
     pds = await resolvePDS(did);
@@ -421,20 +465,7 @@ const RepoView = () => {
                   </Show>
                 </div>
                 <Show when={showPlcLogs()}>
-                  <div class="flex flex-col gap-1 text-sm">
-                    <For each={plcOps()}>
-                      {([entry, diffs]) => (
-                        <div class="flex flex-col">
-                          <span class="text-neutral-500 dark:text-neutral-400">
-                            {localDateFromTimestamp(new Date(entry.createdAt).getTime())}
-                          </span>
-                          {diffs.map((diff) => (
-                            <DiffItem diff={diff} />
-                          ))}
-                        </div>
-                      )}
-                    </For>
-                  </div>
+                  <PlcLogView plcOps={plcOps() ?? []} />
                 </Show>
               </div>
             )}
