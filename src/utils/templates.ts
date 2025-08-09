09ab45de91886963df0bc58c7c12b7d73e5a60ab
Juliet <m@juli.ee>
2025-08-09 21:11:26 +0200

    update deps

--- src/utils/templates.ts
+++ src/utils/templates.ts
@@ -30,7 +30,6 @@ export const uriTemplates: TemplateMap = {
   "com.whtwnd.blog.entry": (uri) => ({
     label: "WhiteWind",
     link: `https://whtwnd.com/${uri.repo}/${uri.rkey}`,
-    icon: "i-lucide-wind",
   }),
   "com.shinolabs.pinksea.oekaki": (uri) => ({
     label: "PinkSea",
