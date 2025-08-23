888a357135ec8998f6d8526c20668bb087cc43a8
Juliet <m@juli.ee>
2025-08-23 23:24:23 +0200

    remove kofi link

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -83,31 +83,22 @@ const Home = () => {
           </A>
         </div>
       </div>
-      <div class="flex justify-between">
-        <div class="flex gap-2">
-          <Tooltip text="GitHub">
-            <A href="https://github.com/notjuliet/pdsls" target="_blank">
-              <div class="i-lucide-github text-xl" />
-            </A>
-          </Tooltip>
-          <Tooltip text="Tangled">
-            <A href="https://tangled.sh/@pdsls.dev/pdsls/" target="_blank">
-              <div class="i-tangled text-xl" />
-            </A>
-          </Tooltip>
-        </div>
-        <div class="flex gap-2">
-          <Tooltip text="Bluesky">
-            <A href="https://bsky.app/profile/did:plc:6q5daed5gutiyerimlrnojnz" target="_blank">
-              <div class="i-tabler-brand-bluesky text-xl" />
-            </A>
-          </Tooltip>
-          <Tooltip text="Donate on Ko-fi">
-            <A href="https://ko-fi.com/notjuliet" target="_blank">
-              <div class="i-lucide-coffee text-xl" />
-            </A>
-          </Tooltip>
-        </div>
+      <div class="flex gap-2">
+        <Tooltip text="GitHub">
+          <A href="https://github.com/notjuliet/pdsls" target="_blank">
+            <div class="i-lucide-github text-xl" />
+          </A>
+        </Tooltip>
+        <Tooltip text="Tangled">
+          <A href="https://tangled.sh/@pdsls.dev/pdsls/" target="_blank">
+            <div class="i-tangled text-xl" />
+          </A>
+        </Tooltip>
+        <Tooltip text="Bluesky">
+          <A href="https://bsky.app/profile/did:plc:6q5daed5gutiyerimlrnojnz" target="_blank">
+            <div class="i-tabler-brand-bluesky text-xl" />
+          </A>
+        </Tooltip>
       </div>
     </div>
   );
