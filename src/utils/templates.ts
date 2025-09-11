71d31348840d16e9e1cee8525ee449078fba5e7b
Juliet <m@juli.ee>
2025-09-11 08:03:38 +0200

    replace bsky icon

--- src/utils/templates.ts
+++ src/utils/templates.ts
@@ -6,22 +6,22 @@ export const uriTemplates: TemplateMap = {
   "app.bsky.actor.profile": (uri) => ({
     label: "Bluesky",
     link: `https://bsky.app/profile/${uri.repo}`,
-    icon: "tabler--brand-bluesky",
+    icon: "ri--bluesky",
   }),
   "app.bsky.feed.post": (uri) => ({
     label: "Bluesky",
     link: `https://bsky.app/profile/${uri.repo}/post/${uri.rkey}`,
-    icon: "tabler--brand-bluesky",
+    icon: "ri--bluesky",
   }),
   "app.bsky.graph.list": (uri) => ({
     label: "Bluesky",
     link: `https://bsky.app/profile/${uri.repo}/lists/${uri.rkey}`,
-    icon: "tabler--brand-bluesky",
+    icon: "ri--bluesky",
   }),
   "app.bsky.feed.generator": (uri) => ({
     label: "Bluesky",
     link: `https://bsky.app/profile/${uri.repo}/feed/${uri.rkey}`,
-    icon: "tabler--brand-bluesky",
+    icon: "ri--bluesky",
   }),
   "fyi.unravel.frontpage.post": (uri) => ({
     label: "Frontpage",
