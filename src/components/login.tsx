530ea8b51ccadeb3d87ffc0bc9129d35ea31ea5b
Kainoa Kanter <kainoa@t1c.dev>
2025-10-18 16:22:56 -0700

    fix: user-plus icon size

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -70,7 +70,7 @@ const Login = () => {
       <div class="dark:bg-dark-100 dark:shadow-dark-700 flex grow items-center gap-2 rounded-lg border-[0.5px] border-neutral-300 bg-white px-2 shadow-xs focus-within:outline-[1px] focus-within:outline-neutral-600 dark:border-neutral-600 dark:focus-within:outline-neutral-400">
         <label
           for="handle"
-          class="iconify lucide--user-round-plus text-neutral-500 dark:text-neutral-400"
+          class="iconify lucide--user-round-plus shrink-0 text-neutral-500 dark:text-neutral-400"
         ></label>
         <input
           type="text"
