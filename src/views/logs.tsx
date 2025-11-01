fc2acefc650550bcf16ee79f9fa309f5858b74a1
Juliet <m@juli.ee>
2025-11-01 16:18:14 +0100

    add gap between item and title

--- src/views/logs.tsx
+++ src/views/logs.tsx
@@ -169,7 +169,7 @@ export const PlcLogView = (props: { did: string }) => {
     return (
       <div
         classList={{
-          "grid grid-cols-[auto_1fr] gap-x-2": true,
+          "grid grid-cols-[auto_1fr] gap-y-0.5 gap-x-2": true,
           "opacity-60": diff.orig.nullified,
         }}
       >
