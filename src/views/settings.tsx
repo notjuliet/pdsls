8b4675c54880164526f82f36bfa5bc12c05ba90b
Juliet <m@juli.ee>
2025-11-17 16:16:26 +0000

    settings padding

--- src/views/settings.tsx
+++ src/views/settings.tsx
@@ -5,7 +5,7 @@ export const [hideMedia, setHideMedia] = createSignal(localStorage.hideMedia ===
 
 const Settings = () => {
   return (
-    <div class="flex w-full flex-col gap-3">
+    <div class="flex w-full flex-col gap-3 px-2">
       <div class="flex items-center gap-1 font-semibold">
         <span>Settings</span>
       </div>
