d7c51d2561d0c1f4aab7994881d18f5548816d89
Juliet <m@juli.ee>
2025-12-25 11:36:51 +0100

    fix border shift on sticky component

--- src/components/sticky.tsx
+++ src/components/sticky.tsx
@@ -29,9 +29,9 @@ export const StickyOverlay = (props: { children?: JSX.Element }) => {
       />
 
       <div
-        class="sticky top-2 z-10 flex w-full flex-col items-center justify-center gap-2 rounded-lg p-3 transition-colors"
+        class="sticky top-2 z-10 flex w-full flex-col items-center justify-center gap-2 rounded-lg border-[0.5px] p-3 transition-colors"
         classList={{
-          "bg-neutral-50 dark:bg-dark-300 border-[0.5px] border-neutral-300 dark:border-neutral-700 shadow-md":
+          "bg-neutral-50 dark:bg-dark-300 border-neutral-300 dark:border-neutral-700 shadow-md":
             filterStuck(),
           "bg-transparent border-transparent shadow-none": !filterStuck(),
         }}
