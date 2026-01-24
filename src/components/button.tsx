974ea40edb2764b194d6be47031fb9a459b3b9fe
Juliet <m@juli.ee>
2026-01-24 14:58:04 +0100

    change light mode button border color

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,7 +17,7 @@ export const Button = (props: ButtonProps) => {
       disabled={props.disabled ?? false}
       class={
         props.class ??
-        "dark:bg-dark-300 dark:hover:bg-dark-200 dark:active:bg-dark-100 flex items-center gap-1 rounded-md border border-neutral-300 bg-neutral-50 px-2.5 py-1.5 text-xs text-neutral-700 transition-colors select-none hover:bg-neutral-100 active:bg-neutral-200 dark:border-neutral-700 dark:text-neutral-300"
+        "dark:bg-dark-300 dark:hover:bg-dark-200 dark:active:bg-dark-100 flex items-center gap-1 rounded-md border border-neutral-200 bg-neutral-50 px-2.5 py-1.5 text-xs text-neutral-700 transition-colors select-none hover:bg-neutral-100 active:bg-neutral-200 dark:border-neutral-700 dark:text-neutral-300"
       }
       classList={props.classList}
       onClick={props.onClick}
